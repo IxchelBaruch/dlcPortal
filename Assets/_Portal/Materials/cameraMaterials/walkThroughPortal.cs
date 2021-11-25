cf171af330f6f8ad8964debf8d34508b3381e881
Alan <aipe84@yahoo.com.mx>
2021-11-24 18:03:31 -0600

    detalles visuales

--- Assets/_Portal/Materials/cameraMaterials/walkThroughPortal.cs
+++ Assets/_Portal/Materials/cameraMaterials/walkThroughPortal.cs
@@ -30,57 +30,29 @@ public class walkThroughPortal : MonoBehaviour
            overlapingPortal = false;
     */
 
-    private void Update()
-    {
-        //print("velocidad: " + playerRBody.velocity);
-    }
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("portalTrigger") && !overlaping)
+        if(other.CompareTag("portalTrigger"))
         {
-            /*overlaping = true;
+
             Vector3 playerFromPortal = transform.InverseTransformPoint(playerObject.transform.position);
 
-            if(playerFromPortal.z <= 0.02)
-            {
-                playerObject.transform.position = otherPortal.position + new Vector3(-playerFromPortal.x, +playerFromPortal.y, -playerFromPortal.z);
+            //if(playerFromPortal.z <= 0.02)
+            //{
+                playerObject.transform.position = otherPortal.position + (otherPortal.forward * 0.2f) - new Vector3(0,1,0);//tp player
 
                 Quaternion cosaMisteriosa = Quaternion.Inverse(transform.rotation) * playerObject.transform.rotation;
                 playerObject.transform.eulerAngles = Vector3.up * (otherPortal.eulerAngles.y - (transform.eulerAngles.y - playerObject.transform.eulerAngles.y) + 180);
                 Vector3 playerCamAngles = Camera.main.transform.localEulerAngles;
                 Camera.main.transform.localEulerAngles = Vector3.right * (otherPortal.eulerAngles.x + Camera.main.transform.localEulerAngles.x);
 
-                Vector3 localPlayerVelocity = thisPortal.InverseTransformPoint(playerObject.GetComponent<Rigidbody>().velocity);
-                playerObject.GetComponent<Rigidbody>().velocity = -otherPortal.transform.forward * playerObject.GetComponent<Rigidbody>().velocity.y * 2;            
+                playerRBody.velocity = playerRBody.velocity.magnitude * otherPortal.forward;
 
-            }*/
+            //}
 
-            /*
-            print("velocidad previa: " + playerRBody.velocity);
 
             
-
-            print("other portal: " + otherPortal.forward);
-
-            float anglex = Vector3.Angle(Vector3.Scale(playerRBody.velocity, new Vector3 (1,0,0)), Vector3.Scale(transform.forward, new Vector3(1, 0, 0)));
-            float angley = Vector3.Angle(Vector3.Scale(playerRBody.velocity, new Vector3(0, 1, 0)), Vector3.Scale(transform.forward, new Vector3(0, 1, 0)));
-            float anglez = Vector3.Angle(Vector3.Scale(playerRBody.velocity, new Vector3(0, 0, 1)), Vector3.Scale(transform.forward, new Vector3(0, 0, 1)));
-
-            print(anglex + " " + angley + " " + anglez);
-
-            */
-
-            //print("velocidad previa: " + playerRBody.velocity);
-
-            /*Vector3 localPlayerVelocity = transform.InverseTransformPoint(playerRBody.velocity);
-            playerRBody.velocity = Vector3.Scale(-otherPortal.transform.forward * 10, localPlayerVelocity);*/
-
-            //print(playerRBody.velocity.magnitude * otherPortal.forward);
-
-            playerRBody.velocity = playerRBody.velocity.magnitude * otherPortal.forward;
-
-            playerObject.transform.position = otherPortal.position + (otherPortal.forward * 0.1f);//tp player
             
             
 
